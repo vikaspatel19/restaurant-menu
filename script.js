53add8e6797211a384ab8fe62e853fa30b936cec
vikaspatel19 <vikasp23.vp@gmail.com>
2024-07-04 23:52:21 +0530

    changes

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+let checkbox ;
+let checkboxesChecked=[];
+let total;
+let totalAmount = 0;
+let sgst;
+let cgst;
 function getCheckBoxes(checkBoxName){
-    let checkbox = document.getElementsByName(checkBoxName);
-    let checkboxesChecked=[];
-    let total = document.getElementById('total');
-    let totalAmount = 0;
-    let sgst = document.getElementById('tax_sgst');
-    let cgst = document.getElementById('tax_cgst');
+    checkbox = document.getElementsByName(checkBoxName);
+    total = document.getElementById('total');
+    
+    sgst = document.getElementById('tax_sgst');
+    cgst = document.getElementById('tax_cgst');
     for(i = 0; i<checkbox.length; i++){
         if(checkbox[i].checked){
             checkboxesChecked.push(checkbox[i].value);                    
@@ -20,4 +25,25 @@ function getCheckBoxes(checkBoxName){
     cgst.innerHTML = (totalAmount*9)/100;
     
     document.getElementById('grand_total').innerHTML = totalAmount + ((totalAmount*9)/100) + (totalAmount*9)/100
+}
+
+function unSelectAll(checkBoxName){
+    checkbox = document.getElementsByName(checkBoxName);
+    checkbox.forEach(function(checkbox_item){
+        checkbox_item.checked = false;
+        checkboxesChecked=[];
+        total.value = 0;
+        sgst.value = 0;
+        cgst.value = 0;
+
+        total.innerHTML = total.value;
+        sgst.innerHTML = sgst.value;
+        cgst.innerHTML = cgst.value;
+
+
+        document.getElementById('grand_total').innerHTML = 0
+
+    } );
+    // checkbox.checked = false;
+    // alert ('Hello');
 }
